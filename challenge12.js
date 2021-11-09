3c88e233ed30e89caa0476153e8e06d5ac62d41b
Franco Emanuel Osman <francoosman12@gmail.com>
2021-11-09 17:35:42 -0300

    subiendo el challenge 12

---
+++ challenge12.js
@@ -0,0 +1,11 @@
+//Area de un triángulo y un circulo
+
+
+let b = prompt("Ingrese la base del triangulo:");
+let a = prompt("Ingrese la altura del triangulo:");
+let r = prompt("Ingrese el radio del circulo:");
+
+
+console.log("El area del triangulo es:" + (b*a)/2);
+console.log("El area del circulo es:" + (3.14*r^2));
+
